485f65f88d14e37f9dd1e84d20c8d1a450a56679
JosephineBonka <josephinebonka123@gmail.com>
2022-03-17 23:36:47 +0300

    more

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,9 @@
  * Auth: Brennan D Baraban
  */
 
-#include "holberton.h"
+#include "main.h"
 
-/**
- * _isupper - Checks for uppercase characters.
- * @c: The character to be checked.
- *
- * Return: 1 if character is uppercase, 0 otherwise.
- */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
